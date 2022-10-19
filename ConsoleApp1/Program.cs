98ede2e0884195b066732f4d5909a245ea83e7b4
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 15:38:37 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //計算數值的階乘10!
+            int Ans = 1;
+            for(int i = 10; i >= 1 ; i--)
+            {
+                Ans = Ans * i;
+            }
+            Console.WriteLine("10!=" + Ans);
+
+        }
+    }
+}
